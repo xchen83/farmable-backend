5440ff7f66dc840d838f525de7957738f853c5e6
Xuan Chen <xchen83@uw.edu>
2025-02-01 14:43:20 -0800

    backend-api

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// Export the Cloudflare Pages function
-export * from './functions/api/products.ts';
+export default {
+  async fetch(request, env) {
+    const { results } = await env.DB.prepare(
+  "SELECT * FROM products"
+).all();
+return Response.json(results);
+
+  },
+};
--- schema.sql
+++ schema.sql
@@ -1,6 +1,9 @@
-CREATE TABLE products (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+DROP TABLE IF EXISTS products;
+CREATE TABLE IF NOT EXISTS products (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   price REAL NOT NULL,
   description TEXT
-); 
+  );
+INSERT INTO products (name, price, description) 
+VALUES ('Frappie Apple', '5.99', 'Frappies favourite');
